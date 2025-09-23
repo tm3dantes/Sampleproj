f4537085056b1265cdafd6cf76f2699950fbe43c
tm3dantes <73610704+tm3dantes@users.noreply.github.com>
2025-09-22 19:23:14 -0700

    Update Main.java
    mod for class

--- Main.java
+++ Main.java
@@ -4,4 +4,6 @@ public static void main (String [] argv)
 System.out.println("hello world");
 System.out.println("modifying");//second mod
   //modifying for 'readme-edits' branch
+  //22Sep25-1922
+  System.out.println("sample for class");
 }
